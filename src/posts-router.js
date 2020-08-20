c4a19c392ddb51672a08ea863e4223445dc8c29e
Minh Nguyen <nguyen.715@osu.edu>
2020-08-19 22:14:16 -0400

    .

--- src/posts-router.js
+++ src/posts-router.js
@@ -12,22 +12,17 @@ postsRouter
   });
 
 postsRouter  
-  .route('/:email')
+  .route('/email/:email')
   .get((req, res) => {
     const db = req.app.get('db');
-    // res.status(200).send(`${req.params.email}`);
+    
     if (req.params.email.length > 0) {
       const email = req.params.email;
       postsService.getPostsByEmail(db, email)
       .then(data => res.status(200).json(data));
     }
-    else if (req.params.token.length > 0) {
-      const token = req.params.token;
-      postsService.getPostsByToken(db, token)
-      .then(data => res.status(200).json(data));
-    }
     else
-      res.status(400).send('Valid email or token is required.');
+      res.status(400).send('Valid email is required.');
   })
   .post(express.json(), (req, res) => {
     const db = req.app.get('db');
@@ -37,5 +32,18 @@ postsRouter
     postsService.insertPost(db, sanitizedPost)
     .then(data => res.status(201).json(data));
   });
+  
+postsRouter  
+  .route('/token/:token')
+  .get((req, res) => {
+    const db = req.app.get('db');
+    if (req.params.token.length > 0) {
+      const token = req.params.token;
+      postsService.getPostsByToken(db, token)
+      .then(data => res.status(200).json(data));
+    }
+    else
+      res.status(400).send('Valid token is required.');
+  })
 
 module.exports = postsRouter;
